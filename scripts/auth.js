05a5a75a9c593eb4f12fd3564ca1f1b9c3e040a9
Mark Watson <watsonmw@gmail.com>
2016-09-27 18:42:49 -0700

    Fix typos in examples.

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,9 +1,9 @@
 // Open the browser, but don't go to the page until we set the username / password
 var b = pizza.open();
 
-// Set username and password to be used for HTTP Authenication
-// This will work with both Basic and Digest HTTP Authernication
+// Set username and password to be used for HTTP Authentication
+// This will work with both Basic and Digest HTTP Authentication
 b.setAuth("httpwatch", utils.randomString());
 
-// Go to a page that requires authenication
+// Go to a page that requires authentication
 b.open("http://www.httpwatch.com/httpgallery/authentication/authenticatedimage/default.aspx");
--- scripts/openasync.js
+++ scripts/openasync.js
@@ -3,5 +3,5 @@ var b = pizza.open();
 // Open the given site and return immediately instead of waiting for the site to load
 b.openAsync("www.yelp.com");
 
-// Wait for a element matching the CSS selector '#button1' (a button with id 'find_desc')
+// Wait for a element matching the CSS selector '#find_desc' (a button with id 'find_desc')
 b.waitForVisible("#find_desc");
