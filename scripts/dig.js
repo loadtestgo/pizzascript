5740f4a02de883ef45c48d3b076f27d7e620348b
ben <bjones993@gmail.com>
2017-03-09 18:35:58 -0800

    Added sample script using DIG to list the Authoriative name servers of a top level domain (TLD)

--- scripts/dig.js
+++ scripts/dig.js
@@ -1,27 +1,26 @@
-var b = pizza.open('https://toolbox.googleapps.com/apps/dig/');
+var b = pizza.open("https://toolbox.googleapps.com/apps/dig/");
 
-pizza.sleep(1000);
-// b.waitPageLoad();  // script has been getting hung up
+b.waitForVisible("#domain");
 
-var domains = ['walmart.com', 'amazon.com'];
+var domains = ["walmart.com", "amazon.com"];
 
-b.type('#domain', domains[Math.floor((Math.random() * domains.length - 1) + 1)]);
+b.type("#domain",utils.randomElement(domains));
 
-var records = ['A', 'AAAA', 'ANY', 'CNAME', 'MX', 'NS', 'PTR', 'SOA', 'SRV', 'TXT'];
-b.click('xpath://input[@value="' + records[5] + '"]');
+var records = ["A", "AAAA", "ANY", "CNAME", "MX", "NS", "PTR", "SOA", "SRV", "TXT"];
+
+// b.click("xpath://input[@value=\"" + records[5] + "\"]");
+b.click("input[value=\"" + records[5] + "\"]");
 
 b.waitForVisible('#response-text');
 
-var html = b.getInnerHTML('xpath://*[@id="response-text"]');
+var html = b.getInnerHTML('#response-text');
 
 var re = /href="(.*?)"/g;
 
 var authNameServers = [];
 
 while(match = re.exec(html)){
-
-    authNameServers.push(match[1].split('@')[1]);
-
+    authNameServers.push(match[1].split("@")[1]);
 }
 
 console.log(authNameServers);
