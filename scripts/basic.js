08bed1c0889e9d9098509ed802da7ea5d8a7b917
ben <bjones993@gmail.com>
2017-03-09 18:23:33 -0800

    Added sample script using DIG to list the Authoriative name servers of a top level domain (TLD)

--- scripts/basic.js
+++ scripts/basic.js
@@ -1,2 +1,7 @@
+// Open the browser and navigate to the requested URL
+// NOTE: A browser instance can be opened without initially
+// setting the URL, see script: sitelogin.js
 b = pizza.open("www.google.com");
+
+// Verify the following text exists on the page
 b.verifyText("Google");
